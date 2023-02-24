2674e52c0431bb0f719fd3edc2541611145609ff
Sam <sam_artola@techexchange.in>
2023-02-24 05:18:09 +0000

    return upload page template when user visits /upload

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -12,3 +12,6 @@ def make_endpoints(app):
         return render_template('main.html')
 
     # TODO(Project 1): Implement additional routes according to the project requirements.
+    @app.route('/upload')
+    def upload():
+        return render_template('/upload.html')
