2fc456fdf0318bfbc970d300099862051b00620f
Mayokun <mayokunoyefeso@techexchange.in>
2023-02-27 03:06:45 +0000

    Add Home and Sign up links to nav bar,and render respective pages when on user's click

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -19,3 +19,7 @@ def make_endpoints(app):
     @app.route('/about')
     def about():
         return render_template('about.html')
+    
+    @app.route('/signup')
+    def signup():
+        return render_template('/signup.html')
